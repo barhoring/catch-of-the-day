e412608668c8fcb7b1e8f9ff1e0c1c3b86115d16
Bar Horing <bar.apple@gmail.com>
2018-10-30 12:43:48 +0200

    making the mama component - App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
 
 // lets mount our app inside div:#main
-render(<StorePicker />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
 // render(<h1>Hello</h1>, document.querySelector('#main'));
-
-export default StorePicker;
