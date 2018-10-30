914627576d209939694526a82880c2f7b03345a8
Bar Horing <bar.apple@gmail.com>
2018-10-30 12:31:19 +0200

    entry form with css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import './css/style.css';
 
 // lets mount our app inside div:#main
 render(<StorePicker />, document.querySelector('#main'));
