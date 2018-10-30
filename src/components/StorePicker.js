3dc9ed18c29484f9f48fb8bc7fb24bdb62bf4ee9
Bar Horing <bar.apple@gmail.com>
2018-10-30 11:52:42 +0200

    initial commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+    render() {
+        return <p>Hello Parsik</p>
+    }
+}
+
+export default StorePicker;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+// lets mount our app inside div:#main
+render(<StorePicker />, document.querySelector('#main'));
+// render(<h1>Hello</h1>, document.querySelector('#main'));
+
+export default StorePicker;
