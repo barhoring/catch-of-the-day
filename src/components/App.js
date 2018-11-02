3094bdca2bed5ec5678ef7c06d60b48e617b6e66
Bar Horing <bar.apple@gmail.com>
2018-11-02 16:01:04 +0200

    load fishes button loads all fishes + add to order button adds to order

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes.js';
 
 class App extends Component {
     state = {
@@ -18,14 +20,43 @@ class App extends Component {
             ()=> console.log('added fish')
         );
     }
+    addToOrder = (fish) => {
+        this.setState(
+            ({ order }) => {
+                order[fish] = order[fish] + 1 || 1;
+                return { order: order }
+            },
+            () => console.log(`added ${fish} to order`)
+        );
+    }
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes});
+    };
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                    {
+                        Object.keys(this.state.fishes).map((key) => {
+                            return (
+                                <Fish
+                                    key={key}
+                                    index={key}
+                                    details={sampleFishes[key]}
+                                    addToOrder={this.addToOrder}
+                                />
+                            )
+                        })
+                    }
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory
+                    addFish={this.addFish}
+                    loadSampleFishes={this.loadSampleFishes}
+                 />
             </div>
         );
     }
