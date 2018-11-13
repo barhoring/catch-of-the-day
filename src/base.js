93ef4047a045948c9e80dc12729ca07fbf04f0a0
Bar Horing <bar.apple@gmail.com>
2018-11-13 23:04:49 +0200

    deploy

--- src/base.js
+++ src/base.js
@@ -1,6 +1,13 @@
-import Rebase from 're-base';
-import firebase from 'firebase';
-import config from './secret.js'
+import Rebase from "re-base";
+import firebase from "firebase";
+// import config from './secret.js'
+
+// in the free tier I know it's not sufe, sigh :(
+const config = {
+  apiKey: "AIzaSyCQXMM0QLESQWtLQEN7yKZGPKcfQkMaOY4",
+  authDomain: "catch-of-the-day-bar-horing.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-bar-horing.firebaseio.com"
+};
 
 const firebaseApp = firebase.initializeApp(config);
 const base = Rebase.createClass(firebaseApp.database());
