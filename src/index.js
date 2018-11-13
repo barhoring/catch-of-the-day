2d401ff1dbc649e2882ab324f768ab76c59cbc43
Bar Horing <bar.apple@gmail.com>
2018-11-13 22:31:28 +0200

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Router from './components/Router';
-import './css/style.css';
-
-// lets mount our app inside div:#main
-render(<Router />, document.querySelector('#main'));
-// render(<h1>Hello</h1>, document.querySelector('#main'));
